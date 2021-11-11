dcbdd7d055b59eaec10afb79563585a0771442ab
QuinnPooch <pucciquinn@gmail.com>
2021-11-11 12:18:49 -0700

    added task audit function and color coding for nearing due dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 var tasks = {};
 
+
+var auditTask = function(taskEl) {
+  
+  var date = $(taskEl).find("span").text().trim();
+
+  var time = moment(date,"L").set("hour", 17);
+ 
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger")
+  }
+  else if (Math.abs(moment().diff(time,"days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -12,6 +29,8 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
+  auditTask(taskLi)
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -39,6 +58,7 @@ var loadTasks = function() {
     });
   });
 };
+
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -241,6 +261,8 @@ $(".list-group").on("change", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
